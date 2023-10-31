4f1440026f71649a27b10078da74a68a6dd947a6
HANHTRAN <hanhtran122002@gmail.com>
2023-11-01 00:19:29 +0700

    Add recommend algorithms

---
+++ src/components/collaborative_filtering.py
@@ -0,0 +1,2 @@
+# collaborative algorithm
+# try to put the code in FUNCTIONS so that we will later call the functions in __init__.py
---
+++ src/components/hybrid.py
@@ -0,0 +1,2 @@
+# hybrid approach
+# try to put the code in FUNCTIONS so that we will later call the functions in __init__.py
